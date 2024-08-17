979b563bd68b0089902cc97c161769b0306c5903
AlexDaGreat555 <alexandertor11667@gmail.com>
2024-08-17 15:45:21 -0400

    added reset and shuffle functions in js file

--- scripts.js
+++ scripts.js
@@ -3,9 +3,16 @@ const cards = document.querySelectorAll('.memory-card');
 
 let hasFlippedCard = false;
 let firstCard, secondCard;
+let lockBoard = false;
+
+cards.forEach(card => card.addEventListener('click', flipCard));
 
 function flipCard()
 {
+    if (lockBoard) return;
+
+    if (this === firstCard) return;
+
     this.classList.add('flip');
 
     if (hasFlippedCard == false)
@@ -20,10 +27,46 @@ function flipCard()
         secondCard = this;
 
         // Do the cards match?
-        if (firstCard.dataset.framework == secondCard.dataset.framework)
-        {
-            console.log('its a match!');
-        }
+        checkForMatch();
     }
 }
-cards.forEach(card => card.addEventListener('click', flipCard));
+
+
+function checkForMatch() {
+    let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+    
+    isMatch ? disableCards() : unflipCards();
+}
+
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+    resetBoard()
+}
+
+function unflipCards() {
+    lockBoard = true;
+
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+
+        lockBoard = false;
+        resetBoard()
+    }, 1000);
+}
+
+function resetBoard()
+{
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+}
+
+(function shuffle()
+{
+    cards.forEach(card => {
+        let randomPos = Math.floor(Math.random() * cards.length);
+        card.style.order = randomPos;
+    });
+})(); // Paranthesis around mean Immediately Invoked Function Expression
+
